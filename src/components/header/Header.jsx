10e5e13cd57dedf9086948e29e05191c41234eca
Sonia Parker <soniaparker1207@gmail.com>
2023-09-19 15:32:31 +1000

    header style changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import Me from '../../assets/me.jpg';
+import Me from '../../assets/me.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>hi</h5>
-        <h1>John Smith</h1>
-        <h5 className="text-light">FE dev</h5>
+        <h5>How is it going?</h5>
+      <h1>John Smith</h1>
+       
+        <h5><span>Qualified</span> UX/UI Designer and <span>Experienced</span> Front-End Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
