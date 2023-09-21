1fabaaf68e00a53ab0e50f4d67a29013cc4fd19c
sonia parker <sonia.parker@ping-works.com.au>
2023-09-21 12:56:22 +1000

    add state hook for nav changes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,15 +3,17 @@ import './nav.css';
 import {FaHome} from 'react-icons/fa';
 import {RiServiceLine, RiUserFill} from 'react-icons/ri';
 import {MdWorkHistory, MdChat} from 'react-icons/md';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a class="active" href="#"><FaHome/></a>
-      <a href="#about"><RiUserFill/></a>
-      <a href="#experience"><MdWorkHistory/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><MdChat/></a>
+      <a onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} href="#"><FaHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUserFill/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdWorkHistory/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdChat/></a>
 
     </nav>
   )
