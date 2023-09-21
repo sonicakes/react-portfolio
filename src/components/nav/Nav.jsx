1f87c68d20e6df0c157429b3099c53266df4787e
sonia parker <sonia.parker@ping-works.com.au>
2023-09-21 11:44:53 +1000

    navigation component - structure and styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './nav.css';
+import {FaHome} from 'react-icons/fa';
+import {RiServiceLine, RiUserFill} from 'react-icons/ri';
+import {MdWorkHistory, MdChat} from 'react-icons/md';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a class="active" href="#"><FaHome/></a>
+      <a href="#about"><RiUserFill/></a>
+      <a href="#experience"><MdWorkHistory/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><MdChat/></a>
+
+    </nav>
   )
 }
 
